9431a7dcb2f0479cdde0df9e49c3e2ff798d4f8c
henry <henryfung721@gmail.com>
2026-06-14 16:54:14 +0800

    main: remove debug toast for autoLaunch

--- main.js
+++ main.js
@@ -129,7 +129,6 @@ function main() {
 
   var navTemplates = navigator.loadNavigationTemplates(config.detection.templateDir);
 
-  toast("autoLaunch=" + settings.autoLaunch);
   if (settings.autoLaunch) {
     floatyMod.updateStatus(panel, "Launching Pikmin Bloom...");
     floatyMod.appendLog(panel, "Launching " + config.app.packageName + "...");
